fc5ad22d7d702a8d0cb13d57b79037737030ac7a
Dainius Puzas <8631224+dsazup@users.noreply.github.com>
2020-11-27 13:03:01 +0000

    demonstrate issue where deleting an element throws a warning

--- app/reflexes/example_reflex.rb
+++ app/reflexes/example_reflex.rb
@@ -3,7 +3,15 @@
 class ExampleReflex < ApplicationReflex
   delegate :uuid, to: :connection
 
-  def test
-    puts "We're live!"
+  before_reflex do
+    session[:rows] ||= []
+  end
+
+  def add
+    session[:rows] << {id: rand(1..100000)}
+  end
+
+  def remove
+    session[:rows] = session[:rows].reject {|row| row[:id] == element.dataset.id.to_i }
   end
 end
